180ccc78eff4f7681ac1af747396e580bb181e25
Julia Alvim <juliameuportoseguro@gmail.com>
2024-05-19 19:08:16 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/Teste.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: red;
+`
+
+function Teste() {
+  return <Botao>Clique Aqui</Botao>
+}
+
+export default Teste
